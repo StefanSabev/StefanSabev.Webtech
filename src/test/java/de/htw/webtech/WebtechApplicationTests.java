f103af1b968a64c3ce3db04e23cff8f3949bf21f
StefanSabev <stefansabev@student.htw-berlin.de>
2022-11-28 14:49:07 +0100

    workflow folder

--- src/test/java/de/htw/webtech/WebtechApplicationTests.java
+++ src/test/java/de/htw/webtech/WebtechApplicationTests.java
@@ -3,6 +3,8 @@ package de.htw.webtech;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//comment for push test
+
 @SpringBootTest
 class WebtechApplicationTests {
 
